302d6b4744ff526efeb649e91b3392766029d716
Jmingpt <88297237+Jmingpt@users.noreply.github.com>
2023-06-07 20:29:11 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 
-def download_csv(df, file_name, file_index=False):
+def download_csv(df, file_name='report', file_index=False):
     csv = df.to_csv(index=file_index).encode('utf-8')
     st.download_button(
         label="Download data as .csv",
@@ -20,7 +20,13 @@ def run():
         layout='centered',  # centered, wide
         initial_sidebar_state='auto',  # auto, expanded, collapsed
     )
-    st.title('Springhill Report')
+    title_cols = st.columns((1, 1))
+    with title_cols[0]:
+        st.title('Springhill Report')
+    with title_cols[1]:
+        st.subheader(' ')
+        st.subheader(' ')
+        date_range_ph = st.empty()
 
     uploaded_file = st.file_uploader(
         label='Select a file',
@@ -30,113 +36,165 @@ def run():
 
     if uploaded_file:
         data = pd.read_excel(uploaded_file, skiprows=2).dropna(subset='Booking Date')
+        data['Booking Date'] = data['Booking Date'].apply(lambda x: x[:-6])
+        data['Booking Date'] = pd.to_datetime(data['Booking Date'])
+        data['Arrival Date'] = pd.to_datetime(data['Arrival Date'])
+        date_range = f"Date Range of dataset: " \
+                     f"**_{min(data['Booking Date']).strftime('%Y-%m-%d')}_ - " \
+                     f"_{max(data['Booking Date']).strftime('%Y-%m-%d')}_**"
+        date_range_ph.markdown(date_range)
         data['Name ID'] = data['Contact Name'].apply(lambda x: x.lower().replace(' ', ''))
+        data = data.drop(['Email', 'Phone No'], axis=1)
         data = data.astype(
             {
-                "Booking No": "string",
-                "Phone No": "string"
+                "Booking No": "string"
             }
         )
 
         # ----------------------------------Return---------------------------------- #
         st.subheader('1. New vs returning customer')
-        df_return = data[data['Status'] != 'Cancelled'].groupby('Name ID')['Booking No'].nunique().sort_values(
-            ascending=False).reset_index()
-        df_return.columns = ['name_id', 'number_of_booking']
-        df_return['status'] = df_return['number_of_booking'].apply(lambda x: 'Return' if x > 1 else 'First-time')
-        df_return = df_return.groupby('status')['name_id'].count().reset_index()
-        df_return.columns = ['status', 'number_of_customer']
+        df_return = data[data['Status'] != 'Cancelled'].groupby('Name ID')['Booking No']\
+                                                       .nunique()\
+                                                       .sort_values(ascending=False)\
+                                                       .reset_index()
+        df_return['Status'] = df_return['Booking No'].apply(lambda x: 'Return' if x > 1 else 'First-time')
+        df_return = df_return.groupby('Status')['Name ID']\
+                             .count()\
+                             .reset_index()
+        df_return.columns = ['Status', 'Number of Guest']
         st.dataframe(
             data=df_return,
             use_container_width=True,
             hide_index=True,
         )
         download_csv(df_return, 'Return Status')
+        st.divider()
 
         # ----------------------------------Room Type---------------------------------- #
         st.subheader('2. Which type of room booked the most?')
-        df_room = data[data['Status'] != 'Cancelled'].groupby('Room Type')['Booking No'].nunique().sort_values(ascending=False).reset_index()
-        df_room.columns = ['room_type', 'number_of_booking']
+        df_room = data[data['Status'] != 'Cancelled'].groupby('Room Type')['Booking No']\
+                                                     .nunique()\
+                                                     .sort_values(ascending=False)\
+                                                     .reset_index()
+        df_room.columns = ['Room Type', 'Number of Booking']
         st.dataframe(
             data=df_room,
             use_container_width=True,
             hide_index=True,
         )
         download_csv(df_room, 'Room Type')
+        st.divider()
 
         # ----------------------------------People---------------------------------- #
         st.subheader('3. What type of people booked the room?')
         df_ppl = data[data['Status'] != 'Cancelled'][['Booking No', 'Room Type', 'NoAdult']].copy()
-        df_ppl['label'] = df_ppl['NoAdult'].apply(lambda x: 'Single' if x == 1 else 'Double' if x == 2 else 'Group')
-        df_ppl.columns = ['booking_id', 'room_type', 'number_of_adult', 'label']
+        df_ppl['People Type'] = df_ppl['NoAdult'].apply(lambda x: 'Single' if x == 1 else 'Double' if x == 2 else 'Group')
+        df_ppl.columns = ['Number of Booking', 'Room Type', 'Number of Adult', 'People Type']
+
+        tmp1_ppl = df_ppl.groupby('People Type')['Number of Booking']\
+                         .nunique()\
+                         .sort_values(ascending=False)\
+                         .reset_index()
+
+        tmp2_ppl = df_ppl.groupby(['People Type', 'Room Type'])['Number of Booking'] \
+            .nunique() \
+            .sort_values(ascending=False) \
+            .reset_index()
         pivot_table = pd.pivot_table(
-            df_ppl.groupby(['label', 'room_type'])['booking_id'].nunique().sort_values(ascending=False).reset_index(),
-            values='booking_id',
-            index='room_type',
-            columns='label',
+            data=tmp2_ppl,
+            values='Number of Booking',
+            index='Room Type',
+            columns='People Type',
             fill_value=0,
             sort=True
         )
+
         st.dataframe(
-            data=df_ppl.groupby('label')['booking_id'].nunique().sort_values(ascending=False).reset_index(),
+            data=tmp1_ppl,
             use_container_width=True,
             hide_index=True,
         )
-        download_csv(df_ppl.groupby('label')['booking_id'].nunique().sort_values(ascending=False).reset_index(), 'Customer')
+        download_csv(tmp1_ppl, 'Customer')
+
         st.dataframe(
             data=pivot_table,
             use_container_width=True,
             hide_index=False,
         )
         download_csv(pivot_table, 'Customer and Room Type', True)
+        st.divider()
 
         # ----------------------------------Time---------------------------------- #
         st.subheader('4. When do they book the room?')
         df_when = data[data['Status'] != 'Cancelled'][['Booking No', 'Booking Date', 'Arrival Date']]
-        df_when['Booking Date'] = df_when['Booking Date'].apply(lambda x: x[:-6])
-        df_when['Booking Date'] = pd.to_datetime(df_when['Booking Date'])
-        df_when['Arrival Date'] = pd.to_datetime(df_when['Arrival Date'])
         df_when['Booking Day'] = df_when['Booking Date'].apply(lambda x: x.strftime('%A'))
         df_when['Arrival Day'] = df_when['Arrival Date'].apply(lambda x: x.strftime('%A'))
         df_when['Booking Week'] = df_when['Booking Day'].apply(lambda x: 'Weekday' if x != 'Saturday' and x != 'Sunday' else 'Weekend')
         df_when['Arrival Week'] = df_when['Arrival Day'].apply(lambda x: 'Weekday' if x != 'Saturday' and x != 'Sunday' else 'Weekend')
-        df_when.columns = ['booking_id', 'booking_date', 'arrival_date', 'booking_day', 'arrival_day', 'booking_week', 'arrival_week']
+        df_when.columns = [
+            'Number of Booking',
+            'booking_date',
+            'arrival_date',
+            'booking_day',
+            'arrival_day',
+            'Booking Day',
+            'Arrival Day'
+        ]
+        df_booking = df_when.groupby('Booking Day')['Number of Booking']\
+                            .nunique()\
+                            .sort_values(ascending=False)\
+                            .reset_index()
+
+        df_arrival = df_when.groupby('Arrival Day')['Number of Booking']\
+                            .nunique()\
+                            .sort_values(ascending=False)\
+                            .reset_index()
         st.dataframe(
-            data=df_when.groupby('booking_week')['booking_id'].nunique().sort_values(ascending=False).reset_index(),
+            data=df_booking,
             use_container_width=True,
             hide_index=True,
         )
-        download_csv(df_when.groupby('booking_week')['booking_id'].nunique().sort_values(ascending=False).reset_index(), 'Booking Time')
+        download_csv(df_booking, 'Booking Day')
+
         st.dataframe(
-            data=df_when.groupby('arrival_week')['booking_id'].nunique().sort_values(ascending=False).reset_index(),
+            data=df_arrival,
             use_container_width=True,
             hide_index=True,
         )
-        download_csv(df_when.groupby('arrival_week')['booking_id'].nunique().sort_values(ascending=False).reset_index(), 'Arrival Time')
+        download_csv(df_arrival, 'Arrival Day')
+        st.divider()
 
         # ----------------------------------Platform---------------------------------- #
         st.subheader('5. Where do they book?')
-        df_platform = data[data['Status'] != 'Cancelled'].groupby('Source')['Booking No'].nunique().sort_values(ascending=False).reset_index()
-        df_platform.columns = ['platform', 'number_of_booking']
+        df_platform = data[data['Status'] != 'Cancelled'].groupby('Source')['Booking No']\
+                                                         .nunique()\
+                                                         .sort_values(ascending=False)\
+                                                         .reset_index()
+        df_platform.columns = ['Platform', 'Number of Booking']
         st.dataframe(
             data=df_platform,
             use_container_width=True,
             hide_index=True,
         )
         download_csv(df_platform, 'Platform')
+        st.divider()
 
         # ----------------------------------Nights---------------------------------- #
         st.subheader('6. How long do they stay?')
         df_night = data[data['Status'] != 'Cancelled'][['Booking No', 'Total night(s)']].copy()
-        df_night['label'] = df_night['Total night(s)'].apply(lambda x: 'More than 1 night' if x > 1 else '1 night')
-        df_night.columns = ['booking_id', 'nights', 'label']
-        df_night = df_night.groupby('label')['booking_id'].nunique().sort_values(ascending=False).reset_index()
+        df_night['Class'] = df_night['Total night(s)'].apply(lambda x: 'More than 1 night' if x > 1 else '1 night')
+        df_night.columns = ['Number of Booking', 'Nights', 'Class']
+        df_night = df_night.groupby('Class')['Number of Booking']\
+                           .nunique()\
+                           .sort_values(ascending=False)\
+                           .reset_index()
         st.dataframe(
             data=df_night,
             use_container_width=True,
             hide_index=True,
         )
         download_csv(df_night, 'Nights')
+        st.divider()
 
         # ----------------------------------Races---------------------------------- #
         st.subheader('7. Races Prediction (Additional)')
@@ -150,8 +208,11 @@ def run():
         df_race['predicted_race'] = races
         df_race = df_race.drop('Contact Name', axis=1)
         df = pd.merge(data, df_race, on='Name ID', how='left')
-        df = df[df['Status'] != 'Cancelled'].groupby('predicted_race')['Booking No'].nunique().sort_values(ascending=False).reset_index()
-        df.columns = ['predicted_race', 'number_of_booking']
+        df = df[df['Status'] != 'Cancelled'].groupby('predicted_race')\
+                                            .agg({'Name ID': 'nunique', 'Booking No': 'nunique'})\
+                                            .sort_values('Booking No', ascending=False)\
+                                            .reset_index()
+        df.columns = ['Predicted Race', 'Number of Guest', 'Number of Booking']
         st.dataframe(
             data=df,
             use_container_width=True,
