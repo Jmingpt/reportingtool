5970e5c766b908f4c8b876dffe177194705ed3e5
Jmingpt <88297237+Jmingpt@users.noreply.github.com>
2023-06-08 11:04:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,23 +3,31 @@ import pandas as pd
 import pickle
 
 
-def download_csv(df, file_name='report', file_index=False):
-    csv = df.to_csv(index=file_index).encode('utf-8')
+def displayNdownload(df, file_name='report', hide_index=True, file_index=False):
+    st.dataframe(
+        data=df,
+        use_container_width=True,
+        hide_index=hide_index,
+    )
+
+    encoded_data = df.to_csv(index=file_index).encode('utf-8')
     st.download_button(
-        label="Download data as .csv",
-        data=csv,
+        label='Download data as .csv',
+        data=encoded_data,
         file_name=f'{file_name}.csv',
         mime='text/csv',
+        key=f'{file_name.lower().replace(" ", "_")}_download'
     )
 
 
 def run():
     st.set_page_config(
         page_title='Reporting Tool',
-        page_icon=None,
+        page_icon="📊",
         layout='centered',  # centered, wide
         initial_sidebar_state='auto',  # auto, expanded, collapsed
     )
+
     title_cols = st.columns((1, 1))
     with title_cols[0]:
         st.title('Springhill Report')
@@ -33,6 +41,7 @@ def run():
         type='xlsx',
         accept_multiple_files=False,
     )
+    st.divider()
 
     if uploaded_file:
         data = pd.read_excel(uploaded_file, skiprows=2).dropna(subset='Booking Date')
@@ -51,7 +60,7 @@ def run():
             }
         )
 
-        # ----------------------------------Return---------------------------------- #
+        # ------------------------------------------Return------------------------------------------ #
         st.subheader('1. New vs returning customer')
         df_return = data[data['Status'] != 'Cancelled'].groupby('Name ID')['Booking No']\
                                                        .nunique()\
@@ -61,31 +70,25 @@ def run():
         df_return = df_return.groupby('Status')['Name ID']\
                              .count()\
                              .reset_index()
+        df_return.loc[df_return.shape[0]] = df_return.sum()
+        df_return.iloc[-1, 0] = 'Grand Total'
         df_return.columns = ['Status', 'Number of Guest']
-        st.dataframe(
-            data=df_return,
-            use_container_width=True,
-            hide_index=True,
-        )
-        download_csv(df_return, 'Return Status')
+        displayNdownload(df_return, 'Return Status')
         st.divider()
 
-        # ----------------------------------Room Type---------------------------------- #
+        # ------------------------------------------Room Type------------------------------------------ #
         st.subheader('2. Which type of room booked the most?')
         df_room = data[data['Status'] != 'Cancelled'].groupby('Room Type')['Booking No']\
                                                      .nunique()\
                                                      .sort_values(ascending=False)\
                                                      .reset_index()
+        df_room.loc[df_room.shape[0]] = df_room.sum()
+        df_room.iloc[-1, 0] = 'Grand Total'
         df_room.columns = ['Room Type', 'Number of Booking']
-        st.dataframe(
-            data=df_room,
-            use_container_width=True,
-            hide_index=True,
-        )
-        download_csv(df_room, 'Room Type')
+        displayNdownload(df_room, 'Room Type')
         st.divider()
 
-        # ----------------------------------People---------------------------------- #
+        # ------------------------------------------People------------------------------------------ #
         st.subheader('3. What type of people booked the room?')
         df_ppl = data[data['Status'] != 'Cancelled'][['Booking No', 'Room Type', 'NoAdult']].copy()
         df_ppl['People Type'] = df_ppl['NoAdult'].apply(lambda x: 'Single' if x == 1 else 'Double' if x == 2 else 'Group')
@@ -95,11 +98,14 @@ def run():
                          .nunique()\
                          .sort_values(ascending=False)\
                          .reset_index()
+        tmp1_ppl.loc[tmp1_ppl.shape[0]] = tmp1_ppl.sum()
+        tmp1_ppl.iloc[-1, 0] = 'Grand Total'
 
-        tmp2_ppl = df_ppl.groupby(['People Type', 'Room Type'])['Number of Booking'] \
-            .nunique() \
-            .sort_values(ascending=False) \
-            .reset_index()
+        tmp2_ppl = df_ppl.groupby(['People Type', 'Room Type'])['Number of Booking']\
+                         .nunique()\
+                         .sort_values(ascending=False)\
+                         .reset_index()
+
         pivot_table = pd.pivot_table(
             data=tmp2_ppl,
             values='Number of Booking',
@@ -109,22 +115,11 @@ def run():
             sort=True
         )
 
-        st.dataframe(
-            data=tmp1_ppl,
-            use_container_width=True,
-            hide_index=True,
-        )
-        download_csv(tmp1_ppl, 'Customer')
-
-        st.dataframe(
-            data=pivot_table,
-            use_container_width=True,
-            hide_index=False,
-        )
-        download_csv(pivot_table, 'Customer and Room Type', True)
+        displayNdownload(tmp1_ppl, 'Customer')
+        displayNdownload(pivot_table, 'Customer and Room Type', False, True)
         st.divider()
 
-        # ----------------------------------Time---------------------------------- #
+        # ------------------------------------------Time------------------------------------------ #
         st.subheader('4. When do they book the room?')
         df_when = data[data['Status'] != 'Cancelled'][['Booking No', 'Booking Date', 'Arrival Date']]
         df_when['Booking Day'] = df_when['Booking Date'].apply(lambda x: x.strftime('%A'))
@@ -144,42 +139,33 @@ def run():
                             .nunique()\
                             .sort_values(ascending=False)\
                             .reset_index()
+        df_booking.loc[df_booking.shape[0]] = df_booking.sum()
+        df_booking.iloc[-1, 0] = 'Grand Total'
 
         df_arrival = df_when.groupby('Arrival Day')['Number of Booking']\
                             .nunique()\
                             .sort_values(ascending=False)\
                             .reset_index()
-        st.dataframe(
-            data=df_booking,
-            use_container_width=True,
-            hide_index=True,
-        )
-        download_csv(df_booking, 'Booking Day')
+        df_arrival.loc[df_arrival.shape[0]] = df_arrival.sum()
+        df_arrival.iloc[-1, 0] = 'Grand Total'
 
-        st.dataframe(
-            data=df_arrival,
-            use_container_width=True,
-            hide_index=True,
-        )
-        download_csv(df_arrival, 'Arrival Day')
+        displayNdownload(df_booking, 'Booking Day')
+        displayNdownload(df_arrival, 'Arrival Day')
         st.divider()
 
-        # ----------------------------------Platform---------------------------------- #
+        # ------------------------------------------Platform------------------------------------------ #
         st.subheader('5. Where do they book?')
         df_platform = data[data['Status'] != 'Cancelled'].groupby('Source')['Booking No']\
                                                          .nunique()\
                                                          .sort_values(ascending=False)\
                                                          .reset_index()
+        df_platform.loc[df_platform.shape[0]] = df_platform.sum()
+        df_platform.iloc[-1, 0] = 'Grand Total'
         df_platform.columns = ['Platform', 'Number of Booking']
-        st.dataframe(
-            data=df_platform,
-            use_container_width=True,
-            hide_index=True,
-        )
-        download_csv(df_platform, 'Platform')
+        displayNdownload(df_platform, 'Platform')
         st.divider()
 
-        # ----------------------------------Nights---------------------------------- #
+        # ------------------------------------------Nights------------------------------------------ #
         st.subheader('6. How long do they stay?')
         df_night = data[data['Status'] != 'Cancelled'][['Booking No', 'Total night(s)']].copy()
         df_night['Class'] = df_night['Total night(s)'].apply(lambda x: 'More than 1 night' if x > 1 else '1 night')
@@ -188,37 +174,31 @@ def run():
                            .nunique()\
                            .sort_values(ascending=False)\
                            .reset_index()
-        st.dataframe(
-            data=df_night,
-            use_container_width=True,
-            hide_index=True,
-        )
-        download_csv(df_night, 'Nights')
+        df_night.loc[df_night.shape[0]] = df_night.sum()
+        df_night.iloc[-1, 0] = 'Grand Total'
+        displayNdownload(df_night, 'Nights')
         st.divider()
 
-        # ----------------------------------Races---------------------------------- #
-#         st.subheader('7. Races Prediction (Additional)')
-#         with open('races_prediction_model.pkl', 'rb') as f:
-#             vec, model = pickle.load(f)
+        # ------------------------------------------Races------------------------------------------ #
+        st.subheader('7. Races Prediction (Additional)')
+        with open('races_prediction_model.pkl', 'rb') as f:
+            vec, model = pickle.load(f)
 
-#         df_race = data[data['Status'] != 'Cancelled'][['Name ID', 'Contact Name']]
-#         name = df_race['Contact Name']
-#         name = vec.transform(name)
-#         races = model.predict(name)
-#         df_race['predicted_race'] = races
-#         df_race = df_race.drop('Contact Name', axis=1)
-#         df = pd.merge(data, df_race, on='Name ID', how='left')
-#         df = df[df['Status'] != 'Cancelled'].groupby('predicted_race')\
-#                                             .agg({'Name ID': 'nunique', 'Booking No': 'nunique'})\
-#                                             .sort_values('Booking No', ascending=False)\
-#                                             .reset_index()
-#         df.columns = ['Predicted Race', 'Number of Guest', 'Number of Booking']
-#         st.dataframe(
-#             data=df,
-#             use_container_width=True,
-#             hide_index=True,
-#         )
-#         download_csv(df, 'Races')
+        tmp_race = data[data['Status'] != 'Cancelled'][['Name ID', 'Contact Name']]
+        name = tmp_race['Contact Name']
+        name = vec.transform(name)
+        races = model.predict(name)
+        tmp_race['predicted_race'] = races
+        tmp_race = tmp_race.drop('Contact Name', axis=1)
+        df_race = pd.merge(data, tmp_race, on='Name ID', how='left')
+        df_race = df_race[df_race['Status'] != 'Cancelled'].groupby('predicted_race')\
+                                                           .agg({'Name ID': 'nunique', 'Booking No': 'nunique'})\
+                                                           .sort_values('Booking No', ascending=False)\
+                                                           .reset_index()
+        df_race.loc[df_race.shape[0]] = df_race.sum()
+        df_race.iloc[-1, 0] = 'Grand Total'
+        df_race.columns = ['Predicted Race', 'Number of Guest', 'Number of Booking']
+        displayNdownload(df_race, 'Races')
 
 
 if __name__ == "__main__":
